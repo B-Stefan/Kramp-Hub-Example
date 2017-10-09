00d70aad70df0639e290270e94a641537057e801
Stefan Bieliauskas <stefan.bie@live.de>
2017-10-09 06:23:29 +0200

    - force spring to use only interface fields
    Signed-off-by: Stefan Bieliauskas <stefan.bie@live.de>

--- src/main/java/kramphub/example/BookMusic/models/IBookMusicEntry.java
+++ src/main/java/kramphub/example/BookMusic/models/IBookMusicEntry.java
@@ -1,6 +1,8 @@
 package kramphub.example.BookMusic.models;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+@JsonSerialize(as=IBookMusicEntry.class)
 public interface IBookMusicEntry {
 
     public String getName();
