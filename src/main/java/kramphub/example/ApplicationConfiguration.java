d4cb9dc07f9484dd47b1584e58332f2ada695eec
Stefan Bieliauskas <stefan.bie@live.de>
2017-10-09 06:58:36 +0200

    - added monitoring for response time of upstream services
    Signed-off-by: Stefan Bieliauskas <stefan.bie@live.de>

--- src/main/java/kramphub/example/ApplicationConfiguration.java
+++ src/main/java/kramphub/example/ApplicationConfiguration.java
@@ -1,17 +1,68 @@
 package kramphub.example;
 
-import org.springframework.beans.factory.annotation.Autowire;
+import com.codahale.metrics.annotation.Gauge;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.*;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.util.StopWatch;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.client.AsyncRestTemplate;
+import org.stagemonitor.core.Stagemonitor;
+import org.stagemonitor.core.metrics.MonitorGauges;
+import org.stagemonitor.core.metrics.metrics2.Metric2Registry;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import static org.stagemonitor.core.metrics.metrics2.MetricName.name;
 
 @Configuration
 public class ApplicationConfiguration {
+
+    /**
+     * Class to track the response time of the external API's
+     * @todo Research about the exact callstack here. Maybe the stopwatch instance get called to early.
+     */
+    public class PerfRequestSyncInterceptor implements AsyncClientHttpRequestInterceptor {
+
+        private final Logger LOG = LoggerFactory.getLogger(PerfRequestSyncInterceptor.class);
+
+        @Override
+        public ListenableFuture<ClientHttpResponse> intercept(HttpRequest request, byte[] body, AsyncClientHttpRequestExecution execution) throws IOException {
+            final StopWatch stopwatch = new StopWatch();
+            stopwatch.start(); // Maybe triggered to early ...
+            ListenableFuture<ClientHttpResponse> response = execution.executeAsync(request, body);
+            final Metric2Registry registry = Stagemonitor.getMetric2Registry();
+            response.addCallback(new ListenableFutureCallback<ClientHttpResponse>() {
+                @Override
+                public void onFailure(Throwable ex) {
+                    stopwatch.stop();
+                    registry.timer(name("itunes_api_request_duration").tag("stage", "itunes").build())
+                            .update(stopwatch.getTotalTimeMillis(), TimeUnit.MILLISECONDS);
+                }
+
+                @Override
+                public void onSuccess(ClientHttpResponse result) {
+                    stopwatch.stop();
+                    registry.timer(name("itunes_api_request_duration").tag("stage", "itunes").build())
+                            .update(stopwatch.getTotalTimeMillis(), TimeUnit.MILLISECONDS);
+
+                }
+            });
+
+            return response;
+        }
+    }
 
     @Bean
     public AsyncRestTemplate asyncRestTemplate(){
@@ -21,6 +72,10 @@ public class ApplicationConfiguration {
 
         AsyncRestTemplate asycTemp = new AsyncRestTemplate();
         asycTemp.getMessageConverters().add(mappingConverter);
+
+        final List<AsyncClientHttpRequestInterceptor> requestInterceptors = new ArrayList<>();
+        requestInterceptors.add(new PerfRequestSyncInterceptor());
+        asycTemp.setInterceptors(requestInterceptors);
 
         return asycTemp;
 
@@ -34,5 +89,7 @@ public class ApplicationConfiguration {
     public ScheduledThreadPoolExecutor poolExecutor() {
       return new ScheduledThreadPoolExecutor(2);
     }
+
+
 
 }
--- src/test/java/kramphub/example/BookMusic/services/ItunesServiceTest.java
+++ src/test/java/kramphub/example/BookMusic/services/ItunesServiceTest.java
@@ -1,16 +1,21 @@
 package kramphub.example.BookMusic.services;
 
 import kramphub.example.BookMusic.models.Album;
-import org.junit.Before;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.AsyncRestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeoutException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,10 +29,36 @@ public class ItunesServiceTest {
     @Test
     public void getAlbumByTitleShouldReturn5Items() throws Exception {
 
-        CompletableFuture<List<Album>> albumFututre = this.itunesService.getAlbumBySearchTerm("my search term");
+        CompletableFuture<List<Album>> albumFututre = this.itunesService.getAlbumBySearchTerm("Sido");
         List<Album> albumList = new ArrayList<>(albumFututre.get());
 
         assertThat(albumList.size()).isEqualTo(5);
+    }
+
+    @Mock
+    AsyncRestTemplate restTemplate;
+    @InjectMocks
+    @Spy
+    private ItunesService itunesServiceMock;
+
+
+    @Test
+    public void test() throws Exception {
+        Mockito.when(restTemplate.getForEntity(
+                Mockito.anyString(),
+                Matchers.any(Class.class)
+        ))
+                .thenAnswer(new Answer<Void>() {
+                    @Override
+                    public Void answer(InvocationOnMock invocation) throws InterruptedException {
+                        Thread.sleep(1000 * 60 );
+
+                        //should cancel before the api respond with anything else to we just need to return something
+                        return null;
+                    }
+                });
+        List<Album> res = itunesServiceMock.getAlbumBySearchTerm("test").get();
+        Assertions.shouldHaveThrown(TimeoutException.class);
     }
 
 }
